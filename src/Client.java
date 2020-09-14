6dac87bfa219f547813074e8b50d7c566ef497b9
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2020-09-14 20:04:39 +0600

    first commit

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,28 @@
+// TODO: 9/14/2020 drawBack is if client runs the programme before server, then it fails to connect 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
+		public static final String Server_ip = "127.0.0.1"; //no matter what,this ip address will work on every computer
+		public static final int Server_port = 9090;
+	public Client(){
+		try {
+			Socket socket = new Socket(Server_ip,Server_port);
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String serverResponse = input.readLine();
+			//here we are ccatching data into an option pane
+			JOptionPane.showMessageDialog(null,serverResponse);
+			socket.close();
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Client DateClient = new Client();
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,33 @@
+/*
+auther : Abdullah Al Masum
+// TODO: 9/14/2020 This is one way simple socket programming where data can transmit server to client
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class Server {
+	public static final int port = 9090;
+	public Server(){
+		try {
+			ServerSocket listener = new ServerSocket(port);
+			System.out.println("Server is waiting for connect...");
+			Socket client = listener.accept();
+			System.out.println("yes...connected");
+			PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+			//we are passing date information here to the client
+			out.println(new Date().toString());
+			System.out.println("Server is about to shut down");
+			client.close();
+			listener.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Server DateServer = new Server();
+	}
 }
